7e596fb83c0713cf3a590bf89a53dc74f156a5d0
jleblanc115 <jleblanc115@gmail.com>
2016-01-30 16:21:52 -0800

    Triggers
    Trigger tiles

---
+++ Tiles/Triggers/Triggers.tsx
@@ -0,0 +1,126 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Triggers" tilewidth="32" tileheight="32" tilecount="41" columns="0">
+ <tile id="3">
+  <image width="32" height="32" source="boss.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="npc01.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="npc02.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="npc03.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="npc04.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="npc05.png"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="32" source="npc06.png"/>
+ </tile>
+ <tile id="10">
+  <image width="32" height="32" source="npc07.png"/>
+ </tile>
+ <tile id="11">
+  <image width="32" height="32" source="npc08.png"/>
+ </tile>
+ <tile id="12">
+  <image width="32" height="32" source="npc09.png"/>
+ </tile>
+ <tile id="13">
+  <image width="32" height="32" source="npc10.png"/>
+ </tile>
+ <tile id="14">
+  <image width="32" height="32" source="npc11.png"/>
+ </tile>
+ <tile id="15">
+  <image width="32" height="32" source="npc12.png"/>
+ </tile>
+ <tile id="16">
+  <image width="32" height="32" source="npc13.png"/>
+ </tile>
+ <tile id="17">
+  <image width="32" height="32" source="npc14.png"/>
+ </tile>
+ <tile id="18">
+  <image width="32" height="32" source="npc15.png"/>
+ </tile>
+ <tile id="19">
+  <image width="32" height="32" source="npc16.png"/>
+ </tile>
+ <tile id="20">
+  <image width="32" height="32" source="npc17.png"/>
+ </tile>
+ <tile id="21">
+  <image width="32" height="32" source="npc18.png"/>
+ </tile>
+ <tile id="22">
+  <image width="32" height="32" source="npc19.png"/>
+ </tile>
+ <tile id="23">
+  <image width="32" height="32" source="npc20.png"/>
+ </tile>
+ <tile id="24">
+  <image width="32" height="32" source="poi01.png"/>
+ </tile>
+ <tile id="25">
+  <image width="32" height="32" source="poi02.png"/>
+ </tile>
+ <tile id="26">
+  <image width="32" height="32" source="poi03.png"/>
+ </tile>
+ <tile id="27">
+  <image width="32" height="32" source="poi4.png"/>
+ </tile>
+ <tile id="28">
+  <image width="32" height="32" source="poi05.png"/>
+ </tile>
+ <tile id="29">
+  <image width="32" height="32" source="poi06.png"/>
+ </tile>
+ <tile id="30">
+  <image width="32" height="32" source="poi07.png"/>
+ </tile>
+ <tile id="31">
+  <image width="32" height="32" source="poi08.png"/>
+ </tile>
+ <tile id="32">
+  <image width="32" height="32" source="poi09.png"/>
+ </tile>
+ <tile id="33">
+  <image width="32" height="32" source="poi10.png"/>
+ </tile>
+ <tile id="34">
+  <image width="32" height="32" source="poi11.png"/>
+ </tile>
+ <tile id="35">
+  <image width="32" height="32" source="poi12.png"/>
+ </tile>
+ <tile id="36">
+  <image width="32" height="32" source="poi13.png"/>
+ </tile>
+ <tile id="37">
+  <image width="32" height="32" source="poi14.png"/>
+ </tile>
+ <tile id="38">
+  <image width="32" height="32" source="poi15.png"/>
+ </tile>
+ <tile id="39">
+  <image width="32" height="32" source="poi16.png"/>
+ </tile>
+ <tile id="40">
+  <image width="32" height="32" source="poi17.png"/>
+ </tile>
+ <tile id="41">
+  <image width="32" height="32" source="poi18.png"/>
+ </tile>
+ <tile id="42">
+  <image width="32" height="32" source="poi19.png"/>
+ </tile>
+ <tile id="43">
+  <image width="32" height="32" source="poi20.png"/>
+ </tile>
+</tileset>
