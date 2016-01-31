7347048ceafb220e86242fb5e361e0c82f6e603c
Tommy <email>
2016-01-30 20:38:44 -0800

    now the game supports talking and collecting memos?

--- main.js
+++ main.js
@@ -18,10 +18,23 @@ var WIDTH = 640;
 var HEIGHT = 480;
 var GRID_SIZE = Game.GRID_SIZE;
 
+var progression = [
+	[
+		{npc: 4},
+		{npc: 14},
+	]
+];
+var curr_progression = 0;
+
 // Game state
 var player = {x: 4, y: 4};
 var grid = [];
 var dialogue = false;
+var shelf = [];
+var inventory = [];
+
+var boss;
+var npcs = [];
 
 window.onload = function() {
 	var c = document.getElementById("myCanvas");
@@ -38,12 +51,8 @@ window.onload = function() {
 	ctx.imageSmoothingEnabled = false;
 */
 
-	ctx.fillText("Hello", 50, 50);
-
-	// Find the appropriate tilemap
-	var layer = TileMaps.desert.layers[0];
-
 	// Create the grid from the tilemap
+	var layer = TileMaps.desert.layers[0];
 	var h = layer.height;
 	var w = layer.width;
 	var map_data = layer.data;
@@ -71,6 +80,25 @@ window.onload = function() {
 		}
 	}
 
+	// Find NPC positions in the tilemap
+	var layer_npc = TileMaps.desert.layers[1];
+	var firstgid = TileMaps.desert.tilesets[1].firstgid;
+	var npc_data = layer_npc.data
+	for(var i=0; i<h; ++i) {
+		for(var j=0; j<w; ++j) {
+			var id = npc_data[j + w*i] - firstgid;
+			var x = j;
+			var y = i;
+
+			if(id === -firstgid) //nobody
+				continue;
+			else if(id === 3) //boss
+				boss = {x: x, y: y};
+			else if(id < firstgid+24) //npc
+				npcs.push({id: id, x: x, y: y});
+		}
+	}
+
 	Game.loadImages(after_images_load(ctx));
 };
 
@@ -84,11 +112,74 @@ var after_images_load = function(ctx) {
 	};
 };
 
+var is_near = function(a1, a2, radius) {
+	if(radius === undefined)
+		radius = 1;
+
+	var dx = a1.x-a2.x;
+	var dy = a1.y-a2.y;
+	return dx*dx + dy*dy < radius * radius;
+};
+
 var keys = {};
 
 var keydown = function(ke) {
+	// Update the 'keys' object.
 	if(ke.keyCode >= 32 && ke.keyCode <= 127)
 		keys[ke.keyCode] = true;
+
+	// Other game-related code ...
+
+	// Upon pressing spacebar ...
+	if(ke.keyCode === 32) {
+		if(dialogue) {
+			dialogue = false;
+		} else {
+			// If player is near NPCs, then open a dialogue box
+			if(is_near(player, boss))
+				talk_to_boss();
+			for(var i=0; i<npcs.length; ++i)
+				if(is_near(player, npcs[i]))
+					talk_to_npc(npcs[i]);
+
+			// If player is near a memo, then take the memo
+			for(var i=0; i<shelf.length; ++i)
+				if(is_near(player, shelf[i])) {
+					var memo = shelf[i];
+					shelf.splice(i, 1);
+					inventory.push(memo);
+					dialogue = 'You got a memo.';
+					break;
+				}
+		}
+	}
+};
+
+var talk_to_boss = function() {
+	dialogue = 'Talking to boss';
+
+	if(shelf.length === 0  &&  inventory.length === 0) {
+		if(curr_progression === progression.length) {
+			// No memos remain
+			dialogue = 'There are no more memos.';
+		} else {
+			// Boss assigns new memos
+			dialogue = 'More memos must be delivered.';
+			var memo_set = progression[curr_progression++];
+			for(var i=0; i<memo_set.length; ++i) {
+				var memo = memo_set[i];
+				memo.x = 10;
+				memo.y = 10+i;
+				shelf.push(memo);
+			}
+		}
+	} else {
+		dialogue = 'You have work to do.';
+	}
+};
+
+var talk_to_npc = function(npc) {
+	dialogue = 'Talking to npc #' + npc.id;
 };
 
 var keyup = function(ke) {
@@ -153,10 +244,6 @@ var onUpdate = function(elapsed) {
 		if(keys[64+19]) //down
 			player.y += speed * elapsed;
 	}
-	//for now, space turns off dialogue when its on.
-	else if(keys[32]) {
-		dialogue = false;
-	}
 
 	Game.collide(grid, player);
 };
@@ -177,9 +264,14 @@ var draw = function(ctx) {
 	var PSIZE = Game.PLAYER_SIZE;
 	ctx.fillRect(player.x * GRID_SIZE, player.y * GRID_SIZE, PSIZE, PSIZE);
 
+	// Draw the memos on the shelf
+	for(var i=0; i<shelf.length; ++i)
+		Game.drawImage(ctx, 'hello.png',
+		               shelf[i].x*GRID_SIZE, shelf[i].y*GRID_SIZE);
+
 	// Text Dialogue Boxes
 	if(dialogue) {
-		dialogueBox(ctx, "lsakjd fdsfsdj flkdsjflks kldsfjf fd jfdfd jfdd jdsks djfjdkfdkej", false);
+		dialogueBox(ctx, dialogue, false);
 	}
 };
 
