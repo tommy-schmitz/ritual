8f77fef43421579e3bcdd2829d0cc7ea8baef9d6
Tommy <email>
2016-01-31 09:47:38 -0800

    oops, I didn't actually add progression.js

---
+++ progression.js
@@ -0,0 +1,33 @@
+window.progression = [
+	[
+		{npc: 5, msg: "memo 1"},
+		{npc: 6, msg: "memo 2"},
+	],
+	[
+		{npc: 7, msg: "memo 3"},
+		{npc: 8, msg: "memo 4"},
+		{npc: 9, msg: "memo 5"},
+	],
+];
+
+window.idle_text = {
+	'4': [  //boss
+		"my first line",
+		"my second line",
+	],
+	'5': [  //daisy
+		"hello1",
+	],
+	'6': [  //daisy
+		"hello2",
+	],
+	'7': [  //daisy
+		"hello3",
+	],
+	'8': [  //daisy
+		"hello4",
+	],
+	'9': [
+		"hello5",
+	],
+};
